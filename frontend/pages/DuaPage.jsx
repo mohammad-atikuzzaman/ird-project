dfb43bb37aee622aa9c7bf2fe5c8496b6616938a
Md Atikuzzaman <akash203037@gmail.com>
2025-01-07 01:16:11 +0600

    build problem fixd

--- frontend/pages/DuaPage.jsx
+++ frontend/pages/DuaPage.jsx
@@ -3,7 +3,7 @@ import DuaCard from "@/components/DuaCard";
 const DuaPage = async ({ posts }) => {
   return (
     <main className="">
-      {posts.map((post, index) => (
+      {posts?.map((post, index) => (
         <DuaCard key={index} sl={index} post={post} />
       ))}
     </main>
